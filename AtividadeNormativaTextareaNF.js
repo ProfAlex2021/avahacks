9d7426877b46f2a60d95c110f3e7e07e7098ce6f
ProfAlex2021 <80551988+ProfAlex2021@users.noreply.github.com>
2024-03-27 18:00:42 -0300

    Atualização: preencher somente se a caixa estiver vazia

--- AtividadeNormativaTextareaNF.js
+++ AtividadeNormativaTextareaNF.js
@@ -3,6 +3,9 @@ var textareas = document.getElementsByTagName('textarea');
 
 // Itera sobre todas as tags <textarea>
 for (var i = 0; i < textareas.length; i++) {
-    // Define o valor "Não Fez" para cada tag <textarea>
-    textareas[i].value = "Não Fez";
+    // Verifica se a tag <textarea> está vazia
+    if (textareas[i].value.trim() === '') {
+        // Define o valor "Não Fez" apenas para tags <textarea> vazias
+        textareas[i].value = "Não Fez";
+    }
 }
